c88fe5425d2bf816955d85f4a9ce9b9a2ff2b300
Douglas Dantas <douglas.f.dantas@outlook.com>
2023-02-23 11:01:25 -0300

    Desafio finalizado

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -7,12 +7,53 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var ballImageView: UIImageView!
+    
     let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
 
+    
+    @IBAction func ask(_ sender: Any) {
+        
+        self.playSound()
+        
+        UIView.animate(withDuration: 0.2) {
+            self.ballImageView.alpha = 0
+        } completion: { finished in
+            self.ballImageView.image = self.ballArray.randomElement()
+            UIView.animate(withDuration: 0.4, delay: 0) {
+                self.ballImageView.alpha = 1
+            }
+        }
+        
+    }
 
+    var player: AVAudioPlayer?
 
+    func playSound() {
+        guard let url = Bundle.main.url(forResource: "aye-aye-sir", withExtension: "mp3") else { return }
+
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+
+            /* The following line is required for the player to work on iOS 11. Change the file type accordingly*/
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+
+            /* iOS 10 and earlier require the following line:
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileTypeMPEGLayer3) */
+
+            guard let player = player else { return }
+
+            player.play()
+
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
+    
 }
 
